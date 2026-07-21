879b2da56b60fa8a8566226f60c5d4ef18faf6da
Aidan Conroy <132592118+aconroy412@users.noreply.github.com>
2026-07-21 17:53:18 -0400

    Lab2 initial

---
+++ examples/Lab2-JavaSyntax/src/com/academy/student/StudentManager.java
@@ -0,0 +1,156 @@
+package com.academy.student;
+
+import java.util.Scanner;
+
+public class StudentManager {
+
+    private static final int MAX_STUDENTS = 20;
+
+    private final Student[] students = new Student[MAX_STUDENTS];
+    private int studentCount = 0;
+    private final Scanner scanner;
+
+    public StudentManager(Scanner scanner) {
+        this.scanner = scanner;
+    }
+
+    public void displayMenu() {
+        System.out.println("====================================");
+        System.out.println("Student Management System");
+        System.out.println("====================================");
+        System.out.println("1. Add Student");
+        System.out.println("2. Display Students");
+        System.out.println("3. Search Student");
+        System.out.println("4. Average Marks");
+        System.out.println("5. Exit");
+        System.out.print("Enter Choice : ");
+    }
+
+    // Methods addStudent, displayStudents, searchStudent, calculateAverage
+    // will be filled in later steps.
+
+    public boolean addStudent() {
+            
+            // Check if max students already
+            if (studentCount >= MAX_STUDENTS) {
+                System.out.println("Class Full");
+                return false;
+            }
+    
+            // Get id
+            String s;
+            do {
+
+                System.out.println("Please tell me the ID you'd like to assign this student: ");
+                s = scanner.nextLine();
+            }
+            while (!s.matches("\\d+"));
+            
+            int id = Integer.parseInt(s);
+    
+            // check if ID exists O(n)
+            for (int i = 0; i < studentCount; i++) {
+                if (id == students[i].getStudentId()) {
+                    System.out.println("ID already exists!");
+                    return false;
+                }
+            }
+    
+            String name;
+            do {
+                System.out.println("What is the student's name?: ");
+                name = scanner.nextLine();
+            }
+            while(name.isBlank() || name.isEmpty());
+    
+            String course;
+            do {
+
+                System.out.println("What is the course they are in?: ");
+                course = scanner.nextLine();
+            }
+            while(course.isBlank() || course.isEmpty());
+    
+
+            double marks;
+
+            do {
+
+                System.out.println("What is their mark (grade) (0 - 100.00");
+                s = scanner.nextLine();
+            }
+            while (!s.matches("\\d+(\\.\\d+)?"));
+            
+            marks = Double.parseDouble(s);
+            if (marks < 0.0 || marks > 100.00) {
+                System.out.println("Invalid marks given, rejected!");
+                return false;
+            }
+
+            Student stu = new Student(id, name, course, marks);
+
+            students[studentCount] = stu;
+            studentCount++;
+            System.out.println("Student Added Successfully");
+
+            return true;
+    }
+
+    public void displayStudents() {
+
+        if (studentCount < 1) {
+            System.out.println("No studetns to display");
+        }
+        for (int i = 0; i < studentCount; i++) {
+            Student student = students[i];
+            System.out.println("-----------------------------------------------------");
+            System.out.printf("%-8s %-20s %-15s %-8s%n",
+                "ID",
+                "Name",
+                "Course",
+                "Marks");
+            System.out.println("-----------------------------------------------------");
+            System.out.printf("%-8d %-20s %-15s %-8.2f%n",
+                student.getStudentId(),
+                student.getName(),
+                student.getCourse(),
+                student.getMarks());
+            System.out.println("-----------------------------------------------------");
+        }
+    }
+
+
+    public void searchStudent() {
+        System.out.println("What is the student's id you're looking for: ");
+        int id = Integer.parseInt(scanner.nextLine());
+
+        for (int i = 0; i < studentCount; i++) {
+            if (this.students[i].getStudentId() == id) {
+                this.students[i].display();
+                return;
+            }
+        }
+
+        System.out.println("Student not found");
+    }
+
+    
+
+    public void calculateAverage() {
+        double top = 0.0;
+        
+        if (studentCount == 0) {
+            System.out.println("No students in list");
+            return;
+        }
+
+        for (int i = 0; i < studentCount; i++) {
+            top += this.students[i].getMarks();
+        }
+
+        double avg = top / studentCount;
+
+        System.out.println("Average Marks: " + avg);
+    }
+}
+
