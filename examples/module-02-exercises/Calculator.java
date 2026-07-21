7dcfa9a9f4713e2720d6cfa586b8b57221c47390
Aidan Conroy <132592118+aconroy412@users.noreply.github.com>
2026-07-21 12:33:04 -0400

    Upgrade mod2 exc1

--- examples/module-02-exercises/Calculator.java
+++ examples/module-02-exercises/Calculator.java
@@ -10,11 +10,28 @@ public class Calculator {
         System.out.print("Second number: ");
         double b = Double.parseDouble(scanner.nextLine());
 
-        System.out.printf("Sum: %.2f%n", a + b);          // addition
-        System.out.printf("Difference: %.2f%n", a - b);   // subtraction
-        System.out.printf("Product: %.2f%n", a * b);      // multiplication
-        System.out.printf("Quotient: %.2f%n", a / b);     // division (double ÷ double)
+        // added a choice feature
+        System.out.print("What would you like to do? (1:add  2:subtract 3:multiply 4:divide) : ");
+        int choice = Integer.parseInt(scanner.nextLine());
 
+        switch(choice) {
+            case 1:
+                System.out.printf("Sum: %.2f%n", a + b);          // addition
+                break;
+            case 2:
+                System.out.printf("Difference: %.2f%n", a - b);   // subtraction
+                break;
+            case 3:
+                System.out.printf("Product: %.2f%n", a * b);      // multiplication
+                break;
+            case 4:
+                System.out.printf("Quotient: %.2f%n", a / b);     // division (double ÷ double)
+                break;
+            default:
+                System.out.println("Invalid number, exiting");
+                break;
+
+        }
         scanner.close();
     }
 }
