22760b6edbd4f03cee67ddadd9e07ea5cc9c16ce
Aidan Conroy <132592118+aconroy412@users.noreply.github.com>
2026-07-23 16:06:01 -0400

    Finish lab 4

---
+++ examples/Lab4-MemoryManagement/GarbageCollectionDemo.java
@@ -0,0 +1,41 @@
+public class GarbageCollectionDemo {
+
+    private static class DemoObject {
+        private final String label;
+        private final byte[] payload = new byte[128];
+
+        DemoObject(String label) {
+            this.label = label;
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println("===== Garbage Collection Demonstration =====");
+        long startTime = System.nanoTime();
+
+        MemoryMonitor.printMemoryReport("Before Allocation");
+
+        DemoObject[] objects = new DemoObject[100000];
+        System.out.println("Creating Objects...");
+        // TODO: fill objects[i] = new DemoObject("Object-" + i)
+
+        int i;
+        for (i = 0; i < 100000; i++) {
+            objects[i] = new DemoObject("Object-" + i);
+        }
+
+
+        // TODO: print Objects Created count; printMemoryReport After Allocation
+
+        System.out.println("Objects created: " + (i) );
+        MemoryMonitor.printMemoryReport("After Allocation");
+
+        // TODO: set objects = null; trigger GC; print After GC report + elapsed ms
+        // Tip: elapsedMillis = (System.nanoTime() - startTime) / 1_000_000
+
+        objects = null;
+        MemoryMonitor.printMemoryReport("After GC");
+        System.out.println("Elapsed time: " + ((System.nanoTime() - startTime) / 1_000_000) );
+        // throw new UnsupportedOperationException("TODO");
+    }
+}
